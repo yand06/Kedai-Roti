dcc8a72c1aa95a251639214060ba86c8bf056885
yand06 <supriyandilaawe@gmail.com>
2023-12-30 23:34:08 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle celas active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Klik di luar toolbar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
